12cd12e685027af08be35e8f3ba650207eb8e5ae
ShamilVR <138783567+ShamilVR@users.noreply.github.com>
2023-07-06 18:32:24 +0600

    Delete pytpyt.py

--- pytpyt.py
+++
@@ -1,9 +0,0 @@
-import netmiko
-import yaml
-
-with open('logpass.yaml') as f:
-    devices = yaml.safe_load(f)
-for dev in devices:
-    ssh = netmiko.ConnectHandler(**dev)
-    ssh.enable()
-    ssh.send_command('sh clock')
